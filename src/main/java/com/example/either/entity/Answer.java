2cfccec6cf19742c589e484a30f5e76998ddddc7
fhdl1221 <fhdl353212@gmail.com>
2025-11-27 09:39:28 +0900

    어노테이션 설정 및 entity 생성

---
+++ src/main/java/com/example/either/entity/Answer.java
@@ -0,0 +1,26 @@
+package com.example.either.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1)
+    private String answerText;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
---
+++ src/main/java/com/example/either/entity/Question.java
@@ -0,0 +1,25 @@
+package com.example.either.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String optionA;
+
+    @Column(nullable = false, length = 200)
+    private String optionB;
+}
